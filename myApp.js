1906ca38cbbdb00a83a3fe9fce1c9e79dcbea831
raBeta <raul.bethencourt.pro@gmail.com>
2022-09-24 18:47:10 +0200

    :construction: progress

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 // connection to database
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
